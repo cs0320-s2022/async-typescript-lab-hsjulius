1be0162a7ca38ff2a3492f9ede497462a0f275d9
hsjulius <60826854+hsjulius@users.noreply.github.com>
2022-03-06 23:32:09 -0500

    feat: selection working

--- src/main/java/edu/brown/cs/student/main/Main.java
+++ src/main/java/edu/brown/cs/student/main/Main.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import com.google.common.collect.ImmutableMap;
@@ -14,6 +15,8 @@ import freemarker.template.Configuration;
 import joptsimple.OptionParser;
 import joptsimple.OptionSet;
 import joptsimple.OptionSpec;
+import org.json.JSONException;
+import org.json.JSONObject;
 import spark.*;
 import spark.template.freemarker.FreeMarkerEngine;
 
@@ -23,7 +26,7 @@ import spark.template.freemarker.FreeMarkerEngine;
  */
 public final class Main {
 
-  private static final int DEFAULT_PORT = 4567;
+  private static final int DEFAULT_PORT = 1212;
 
   /**
    * The initial method called when execution begins.
@@ -80,6 +83,7 @@ public final class Main {
     // Allows requests from any domain (i.e., any URL). This makes development
     // easier, but it’s not a good idea for deployment.
     Spark.before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
+    Spark.post("/route", new ResultsHandler());
   }
 
   /**
@@ -101,23 +105,29 @@ public final class Main {
 
   /**
    * Handles requests for horoscope matching on an input
-   * 
+   *
    * @return GSON which contains the result of MatchMaker.makeMatches
    */
   private static class ResultsHandler implements Route {
     @Override
-    public String handle(Request req, Response res) {
+    public String handle(Request req, Response res) throws JSONException {
+      JSONObject json = new JSONObject(req.body());
+      String sunVal = json.getString("sun");
+      String moonVal = json.getString("moon");
+      String risingVal = json.getString("rising");
       // TODO: Get JSONObject from req and use it to get the value of the sun, moon,
       // and rising
       // for generating matches
 
       // TODO: use the MatchMaker.makeMatches method to get matches
 
+      List<String> matches = MatchMaker.makeMatches(sunVal, moonVal, risingVal);
       // TODO: create an immutable map using the matches
 
+      ImmutableMap<String, Object> matchesMap = ImmutableMap.of("response", matches);
       // TODO: return a json of the suggestions (HINT: use GSON.toJson())
       Gson GSON = new Gson();
-      return null;
+      return GSON.toJson(matchesMap);
     }
   }
 }
--- src/main/site/ts/main.ts
+++ src/main/site/ts/main.ts
@@ -1,27 +1,57 @@
 // TODO: select the list element where the suggestions should go, and all three dropdown elements
 //  HINT: look at the HTML
-
+const suggestions = document.querySelector('#suggestions') as HTMLInputElement
 // Here, when the value of sun is changed, we will call the method postAndUpdate.
 // TODO: Do the same for moon and rising
+const sunDropdown = document.querySelector('#sun') as HTMLSelectElement
+sunDropdown.addEventListener("change", postAndUpdate)
+const moonDropdown = document.querySelector('#moon') as HTMLSelectElement
+moonDropdown.addEventListener("change", postAndUpdate)
+const risingDropdown = document.querySelector('#rising') as HTMLSelectElement
+risingDropdown.addEventListener("change", postAndUpdate)
 
-// TODO: Define a type for the request data object here.
-// type MatchesRequestData = {}
+// TODO: lDefine a type for the request data object here.
+type MatchesRequestData = { sun: string, moon: string, rising: string, }
 
 // TODO: Define a type for the response data object here.
-// type Matches = {}
+type Matches = { response: string[]}
 
 function postAndUpdate(): void {
+  suggestions.innerHTML = ''
   // TODO: empty the suggestionList (you want new suggestions each time someone types something new)
   //  HINT: use .innerHTML
 
   // TODO: add a type annotation to make this of type MatchesRequestData
-  const postParameters = {
+  const postParameters : MatchesRequestData = {
+    sun: sunDropdown.value,
+    moon: moonDropdown.value,
+    rising: risingDropdown.value,
+
     // TODO: get the text inside the input box
     //  HINT: use sun.value to get the value of the sun field, for example
   };
 
   console.log(postParameters)
 
+  fetch("http://localhost:1212/route", {
+    // Request method
+    method: 'post',
+    body: JSON.stringify(postParameters),
+    // Data in JSON format to send in the request
+    // body: JSON.stringify({
+    //   title: 'POST Requests are Awesome!',
+    //   body: 'This is why POST requests are awesome!',
+    //   userId: 1,
+    // }),
+    // HTTP headers to tell the receiving server what format the data is in
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    }
+  })
+      .then((response) => response.json())
+      // .then((data) => console.log(data))
+      .then((data: Matches) => updateSuggestions(data.response))
+      .catch((error: any) => console.error("Error:", error));
   // TODO: make a POST request using fetch to the URL to handle this request you set in your Main.java
   //  HINT: check out the POST REQUESTS section of the lab and of the front-end guide.
   //  Make sure you add "Access-Control-Allow-Origin":"*" to your headers.
@@ -33,12 +63,25 @@ function postAndUpdate(): void {
 }
 
 function updateSuggestions(matches: string[]): void {
+  console.log(matches)
+  for (let i = 0; i < matches.length; i++) {
+    suggestions.innerHTML += `<li tabindex=i>${matches[i]}<li>`;
+  }
   // TODO: for each element in the set of matches, append it to the suggestionList
   //  HINT: use innerHTML += to append to the suggestions list
   //  NOTE: you should use <li> (list item) tags to wrap each element. When you do so,
   //  make sure to add the attribute 'tabindex="0"' (for example: <li tabindex="0">{your element}</li>).
   //  This makes each element selectable via screen reader.
 }
+
+document.addEventListener("keyup", async (event) => { if(event.key != "k") {
+  postAndUpdate()
+}})
+
+document.addEventListener("keyup", async (event) => { if(event.key === "k" ) {
+  await updateValues("Scorpio", "Taurus", "Pisces")
+  postAndUpdate()
+}})
 
 // TODO: create an event listener to the document (document.addEventListener) that detects "keyup".
 //  When a certain key of your choice is clicked, reset the values of sun, moon, and rising to your own
@@ -51,7 +94,7 @@ async function updateValues(sunval: string, moonval: string, risingval: string):
   // It's unnecessary here, but it simulates asynchronous behavior you often have to account for.
   await new Promise(resolve => setTimeout(resolve, 1000));
 
-  sun.value = sunval;
-  moon.value = moonval;
-  rising.value = risingval;
+  sunDropdown.value = sunval;
+  moonDropdown.value = moonval;
+  risingDropdown.value = risingval;
 }
